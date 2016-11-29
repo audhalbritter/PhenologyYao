6a4733c1047b73fa0cdcdc90c4e1bf5f70e2b66a
audhalbritter <me@audhalbritter.com>
2016-11-29 08:46:59 +0100

    import flowering data

--- TraitData.R
+++ TraitData.R
@@ -1,5 +1,6 @@
 # Trait data
 trait <- read_excel("plant functional groups-Snow Fence.xlsx", col_names = TRUE)
+trait <- trait[,c(1,3)]
 head(trait)
 
 
@@ -9,5 +10,9 @@ head(trait)
 ### Flowering time
 
 pheno.long %>% 
-  filter(treatment == "Control", pheno.stage == "b") %>% 
+  filter(treatment == "Control", pheno.stage == "Flower", pheno.var == "first") %>% 
   ggplot(aes(x = value)) + geom_histogram()
+
+flowertime <- read_excel("FloweringClassification.xlsx", col_names = TRUE)
+flowertime <- flowertime[,1:2]
+head(flowertime)
