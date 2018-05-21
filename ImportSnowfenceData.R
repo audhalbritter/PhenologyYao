8aa1f9b82d40b1394296021aab34b1c7024b2d7e
audhalbritter <audhalbritter.com>
2018-05-21 09:04:12 +0200

    import root CN for 2015

--- ImportSnowfenceData.R
+++ ImportSnowfenceData.R
@@ -2,45 +2,79 @@ library("tidyverse")
 library("readxl")
 library("broom")
 
+pn <- . %>% print(n = Inf)
+
 ### Soil CNP
 cnp <- read_excel(path = "Data/soil data_CNP_2015.xlsx", sheet = 1)
 
 cnp <- cnp %>% 
   rename(depth = `depth(cm)`, total_C = `total C（g/kg）`, organic_C = `organic C（g/kg）`, total_N = `total N（g/kg）`, available_N = `available N（mg/kg）`, total_P = `total P(%)`, available_P = `available P（mg/kg）`) %>% 
-  gather(key = variable, value = value, total_C, organic_C, total_N, available_N, total_P, available_P)
-
+  gather(key = variable, value = value, total_C, organic_C, total_N, available_N, total_P, available_P) %>% 
+  mutate(newT = case_when(
+    snow == "SH" & depth == "0-10" ~ "SH-1-10",
+    snow == "SH" & depth == "10-20" ~ "SH-10-20",
+    snow == "CK" & depth == "0-10" ~ "CK-1-10",
+    snow == "CK" & depth == "10-20" ~ "CK-10-20"
+  ))
 
 cnp %>% 
-  ggplot(aes(x = snow, y = value)) +
+  ggplot(aes(x = newT, y = value, fill = snow)) +
   geom_boxplot() +
-  facet_wrap(~ variable, scales = "free_y")
+  facet_wrap( ~ variable, scales = "free_y")
 
 dfCNP <- cnp %>% 
-  group_by(variable) %>% 
+  group_by(variable, depth) %>%
   do(fit = lm(value ~ snow, data = .))
-
-tidy(dfCNP, fit)
+  
+tidy(dfCNP, fit) %>% 
+  filter(p.value < 0.05, term != "(Intercept)")
 
 
 
 ### Ingrowth core
-ingrowth <- read_excel(path = "Data/root CN_2016_ingrowth core.xls", sheet = 1)
-ingrowth <- ingrowth %>% 
-  rename(total_C = `total C(g/kg)`, total_N = `total N(g/kg)`) %>% 
+# 2016
+ingrowth16 <- read_excel(path = "Data/root CN_2016_ingrowth core.xls", sheet = 1)
+ingrowth16 <- ingrowth16 %>% 
+  rename(plot = `Plot#`, total_C = `total C(g/kg)`, total_N = `total N(g/kg)`) %>% 
   mutate(depth = plyr::mapvalues(depth, c("0~10", "10~20"), c("0-10", "10-20"))) %>% 
-  gather(key = variable, value = value, total_C, total_N)
+  gather(key = variable, value = value, total_C, total_N) %>% 
+  mutate(year = 2016) %>% 
+  mutate(newT = case_when(
+    treatment == "S" & depth == "0-10" ~ "S-1-10",
+    treatment == "S" & depth == "10-20" ~ "S-10-20",
+    treatment == "C" & depth == "0-10" ~ "C-1-10",
+    treatment == "C" & depth == "10-20" ~ "C-10-20"
+  ))
 
 
-ggplot(ingrowth, aes( x = treatment, y = value)) +
+# 2015 and bind rows
+ingrowth15 <- read_excel(path = "Data/RootCN_2015_using augar.xlsx", sheet = 1)
+ingrowth <- ingrowth15 %>% 
+  rename(treatment = Treatment, plot = `Plot#`, mass = `mass(mg)`, total_N = `total N(g/kg)`, total_C = `total carbon(g/kg)`, depth = `Depthe(cm)`) %>% 
+  select(-X__1, -X__2, -X__3) %>% 
+  gather(key = variable, value = value, total_C, total_N, mass) %>% 
+  mutate(year = 2015) %>% 
+  mutate(newT = case_when(
+    treatment == "S" & depth == "0-10" ~ "S-1-10",
+    treatment == "S" & depth == "10-20" ~ "S-10-20",
+    treatment == "C" & depth == "0-10" ~ "C-1-10",
+    treatment == "C" & depth == "10-20" ~ "C-10-20"
+  )) %>% 
+  bind_rows(ingrowth16)
+
+
+ggplot(ingrowth, aes( x = newT, y = value, fill = treatment)) +
   geom_boxplot() +
   labs(x = "", y = "Total C or N in g/kg") +
-  facet_wrap(~variable, scales = "free_y")
+  facet_grid(year ~ variable, scales = "free_y")
 
 dfingrowth <- ingrowth %>% 
-  group_by(variable) %>% 
+  group_by(variable, depth, year) %>% 
   do(fit = lm(value ~ treatment, data = .))
 
-tidy(dfingrowth, fit)
+tidy(dfingrowth, fit) %>% pn
+
+
 
 
 
