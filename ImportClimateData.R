d8123fb53e265223471158456b5c5b51ffd73281
audhalbritter <audhalbritter.com>
2018-05-22 13:25:29 +0200

    import 2015 and 2016 weather data

--- ImportClimateData.R
+++ ImportClimateData.R
@@ -1,12 +1,11 @@
 # Import Climate Data
-
-
 # read libraries
 library("tidyr")
 library("dplyr")
 library("lubridate")
 library("ggplot2")
 library("readxl")
+library("data.table")
 
 ### SOIL TEMPERATURE DATA
 # Function to read in snowfence data
@@ -34,6 +33,8 @@ snowfence2 <- snowfence %>%
   mutate(Tsoil10 = ifelse(Tsoil10 > 25 | Tsoil10 < -10, NA, Tsoil10)) %>% 
   mutate(waterContent5 = ifelse(waterContent5 > 1000 , NA, waterContent5))
 
+snowfence2 <- setDT(snowfence2)
+
 #summary(snowfence2)  
 ggplot(snowfence2, aes(x = dateTime, y = Tsoil10)) + geom_line() + facet_wrap(~distance) + geom_vline(xintercept = as.numeric(ymd_hms("2016-03-01 00:00:01")), color = "red") +
  geom_vline(xintercept = as.numeric(ymd_hms("2016-05-01 00:00:01")), color = "blue")
@@ -41,6 +42,48 @@ ggplot(snowfence2, aes(x = dateTime, y = Tsoil10)) + geom_line() + facet_wrap(~d
 head(snowfence2)
 dim(snowfence)
 
+# Caclculate daily data
+dailyData <- snowfence2 %>% 
+  group_by(dateTime) %>% filter(row_number(dateTime) == 1) %>%
+  gather(key = variable, value = value, Tsoil5, Tsoil10, Tsoil20, waterContent5, waterContent10, waterContent20) %>% 
+  mutate(day = dmy(format(dateTime, "%d.%b.%Y"))) %>%
+  group_by(day, variable, distance) %>%
+  summarise(n = n(), mean = mean(value, na.rm = TRUE), se = sd(value, na.rm = TRUE)/sqrt(n))
+
+
+dailyData %>% 
+  select(-n, -se) %>% 
+  spread(key = distance, value = mean) %>% 
+  mutate(Diff2.5 = `2.5m` - Control, Diff5 = `5m` - Control) %>% 
+  gather(key = Diffs, value = value, Diff2.5, Diff5) %>% 
+  filter(value > -10) %>% 
+  ggplot(aes(x = day, y = value, colour = Diffs)) +
+  geom_line()
+
+dailyData %>% 
+  filter(distance %in% c("Control", "2.5m"), mean < 60, variable == "Tsoil5") %>% 
+  ggplot(aes(x = day, y = mean, colour = distance)) +
+  labs(x = "", y = "mean daily temperature in °C") +
+  geom_line()
+
+
+
+# Calculate monthly data
+monthlyData <- dailyData %>% 
+  filter(!is.na(mean)) %>%
+  mutate(date = dmy(paste0("15-",format(day, "%b.%Y")))) %>%
+  group_by(date, variable, distance) %>%
+  summarise(n = n(), value = mean(mean), se = sd(mean)/sqrt(n)) 
+  
+monthlyData %>% 
+  filter(variable == "Tsoil10", distance %in% c("Control", "2.5m")) %>% 
+  filter(n > 16) %>% 
+  ggplot(aes(x = date, y = value, ymin = value - se, ymax = value + se, colour = distance)) +
+  geom_line() +
+  geom_point() +
+  geom_errorbar() +
+  labs(x = "", y = "Monthly temperature in °C")
+ggsave(MonthlyTemp, filename = "Figures/MonthlyTemp.jpg", width = 8, height = 5, dpi = 300)
 
 ### SNOWDEPTH DATA
 snowdepth <- read_excel("/Volumes/FELLES/MATNAT/BIO/Ecological and Environmental Change/TransplantChina/ClimateData/Snowfence/SnowDepth_Gongga.xlsx", col_names = TRUE, col_types = c("date", "numeric", "numeric", "numeric", "numeric", "numeric", "date"))
@@ -59,3 +102,27 @@ ggplot(snowdepth, aes(x = TIMESTAMP, y = Snow_Depth_1)) + geom_line() + ylim(-0.
 
 
 
+
+control <- read_excel("Data/weather data in 2016.xlsx", col_names = TRUE, sheet = 1)
+snow <- read_excel("Data/weather data in 2016.xlsx", col_names = TRUE, sheet = 2)
+
+snow <- snow %>% 
+  mutate(Treatment = "Snow") %>% 
+  rename(Temperature_5 = `Tem-5cm`, Temperature_10 = `Tem-10cm`, Temperature_20 = `Tem-20cm`, Moisture_5 = `Mois-5cm`, Moisture_10 = `Tem-10cm__1`, Moisture_20 = `Tem-20cm__1`)
+
+control %>% 
+  mutate(Treatment = "Control") %>% 
+  rename(Date = date, Temperature_5 = `Tem-5cm`, Temperature_10 = `Tem-10cm`, Temperature_20 = `Tem-20cm`, Moisture_5 = `Mois-5cm`, Moisture = `Mois-10cm`, Moisture_20 = `Mois-20cm`) %>% 
+  bind_rows(snow) %>% 
+  gather(key = Variable, value = Value, - Date, -Treatment) %>% 
+  filter(Variable %in% c("Temperature_5", "Temperature_10", "Temperature_20")) %>% 
+  ggplot(aes(x = Date, y = Value,  colour = Treatment)) +
+    geom_line() +
+  facet_wrap(~ Variable, ncol = 1)
+  
+
+   
+    
+
+ggplot(weather, aes(x = date, y = `Tem-5cm`)) +
+  geom_line()
